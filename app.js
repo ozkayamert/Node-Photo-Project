0d901eb83a763c38e32448022bb64d0c52b72546
Mert Ozkaya <ozkayamert35@gmail.com>
2025-02-04 22:40:50 +0300

    Nodemon

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('Function is working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    "name": "photo name",
+    "description": "photo description",
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
